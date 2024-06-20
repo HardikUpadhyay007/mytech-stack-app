f5b05365e681e049a5521020b010559f6f5ce2d8
Hardik Upadhyay <119848774+HardikUpadhyay007@users.noreply.github.com>
2024-06-21 00:06:44 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,81 @@
+import htmlImage from "./assests/html.jpeg";
+import cssImage from "./assests/css.jpeg";
+import jsImage from "./assests/js.jpg";
+import bootImage from "./assests/bootstrap.jpg";
+import angularImage from "./assests/angular.jpeg";
+import reactImage from "./assests/react.jpeg";
+import tailImage from "./assests/tailwind.png";
+import cppImage from "./assests/cpp.jpeg";
+import javaImage from "./assests/java.png";
+import pythonImage from "./assests/python.jpeg";
+import nodeImage from "./assests/nodejs.png";
+
+const tech = [
+    {
+        id: 1,
+        name: "HTML5",
+        image: htmlImage,
+        text: "HTML5 is the latest version of the Hypertext Markup Language, providing enhanced support for multimedia, semantic elements, and advanced web applications while ensuring backward compatibility.",
+    },
+    {
+        id: 2,
+        name: "CSS3",
+        image: cssImage,
+        text: "CSS3 is the latest evolution of the Cascading Style Sheets language, introducing new features for enhanced design, including advanced animations, transitions, flexbox, grid layouts, and media queries for responsive web development.",
+    },
+    {
+        id: 3,
+        name: "JAVASCRIPT",
+        image: jsImage,
+        text: "JavaScript is a versatile, high-level programming language commonly used in web development to create dynamic and interactive user experiences by manipulating HTML and CSS, and handling events on web pages.",
+    },
+    {
+        id: 4,
+        name: "BOOTSTRAP",
+        image: bootImage,
+        text: "Bootstrap is a popular open-source CSS framework that simplifies web development by providing a collection of responsive design templates and pre-built components for creating modern, mobile-first websites.",
+    },
+    {
+        id: 5,
+        name: "TAILWIND",
+        image: tailImage,
+        text: "Tailwind CSS is a utility-first CSS framework that offers a highly customizable set of utility classes, enabling rapid and responsive design directly in your HTML without writing traditional CSS.",
+    },
+    {
+        id: 6,
+        name: "REACT JS",
+        image: reactImage,
+        text: "React.js is a JavaScript library for building user interfaces, focusing on creating reusable UI components and managing the state efficiently with a virtual DOM for dynamic and high-performance web applications.",
+    },
+    {
+        id: 7,
+        name: "ANGULAR JS",
+        image: angularImage,
+        text: "AngularJS is a structural framework for dynamic web applications, enabling developers to use HTML as the template language and extend its syntax to express application components clearly and succinctly.",
+    },
+    {
+        id: 8,
+        name: "NODE JS",
+        image: nodeImage,
+        text: "Node.js is a runtime environment built on Chrome's V8 JavaScript engine, enabling server-side scripting to build scalable, high-performance network applications using JavaScript.",
+    },
+    {
+        id: 9,
+        name: "C++",
+        image: cppImage,
+        text: "C++ is a powerful, high-performance programming language that extends C by adding object-oriented features, enabling the development of complex software systems, applications, and game engines.",
+    },
+    {
+        id: 10,
+        name: "JAVA",
+        image: javaImage,
+        text: "Java is a versatile, object-oriented programming language designed for portability and high performance, widely used for building cross-platform applications, from mobile apps to large-scale enterprise systems.",
+    },
+    {
+        id: 11,
+        name: "PYTHON",
+        image: pythonImage,
+        text: "Python is a versatile, high-level programming language known for its simplicity and readability, used for web development, data analysis, artificial intelligence, scripting, and automation tasks.",
+    },
+];
+export default tech;
